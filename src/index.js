541f61b8f812f0665a2b1d79d0fbcd5eb13d07a2
Hélder <helderafonsoss@hotmail.com>
2020-01-06 19:22:46 -0300

    Configuração inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import a from '~/index';
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+      <Text>Teste2</Text>
+    </View>
+  );
+}
