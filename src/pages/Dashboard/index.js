0b501ec84178283dc3f3e1d2224af059934d8696
Hélder <helderafonsoss@hotmail.com>
2020-01-11 10:40:25 -0300

    Criação dos agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import { Container, Title, List } from './styles';
+
+const data = [1, 2, 3, 4]
 
 export default function Dashboard() {
   return (
-    <Background />
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => (
+            <Appointment />
+          )}
+        />
+      </Container>
+    </Background>
   );
 }
 
