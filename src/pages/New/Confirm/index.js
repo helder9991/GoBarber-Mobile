05bf30602766742fa463af437ad231992348f5a7
Hélder <helderafonsoss@hotmail.com>
2020-01-12 23:59:27 -0300

    Criação da pagina de agendamento

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function Confirm() {
+import api from '~/services/api';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation }) {
+
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt })
+    ,[time])
+
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time
+    });
+
+    navigation.navigate('Dashboard');
+  }
   return (
-    <View />
+    <Background>
+      <Container>
+       <Avatar
+          source={{
+            uri: provider.avatar
+            ? `http://192.168.1.106:3333/files/${provider.avatar.path}`
+            : `http://api.adorable.io/avatar/50/${provider.name}.png`
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar Agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
   );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar Agendamento',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  )
+})
